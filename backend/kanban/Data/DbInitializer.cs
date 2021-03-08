b0b1180a0fce095b6046b617f9c05734568eecff
Erik Szabó <erik1szabo1@gmail.com>
2021-03-08 19:22:28 +0100

    using migrations instead of ensurecreated

--- backend/kanban/Data/DbInitializer.cs
+++ backend/kanban/Data/DbInitializer.cs
@@ -8,8 +8,6 @@ namespace kanban.Data
     {
         public static void Initialize(KanbanContext context)
         {
-            context.Database.EnsureCreated();
-
             if (context.Columns.Any()) return;
 
             var columns = new Column[]
@@ -23,18 +21,6 @@ namespace kanban.Data
             foreach (var column in columns)
                 context.Columns.Add(column);
 
-            context.SaveChanges();
-
-            context.Cards.Add(new Card { ColumnID = 1, Title = "Teszt 11", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 1});
-            context.Cards.Add(new Card { ColumnID = 1, Title = "Teszt 12", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 2 });
-            context.Cards.Add(new Card { ColumnID = 1, Title = "Teszt 13", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 3 });
-
-            context.Cards.Add(new Card { ColumnID = 2, Title = "Teszt 21", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 1 });
-            context.Cards.Add(new Card { ColumnID = 2, Title = "Teszt 22", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 2 });
-            context.Cards.Add(new Card { ColumnID = 2, Title = "Teszt 23", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 3 });
-
-            context.Cards.Add(new Card { ColumnID = 3, Title = "Teszt 31", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 1 });
-            context.Cards.Add(new Card { ColumnID = 3, Title = "Teszt 32", Description = "Teszt leírás", Deadline = DateTime.Now, Sort = 2 });
             context.SaveChanges();
         }
     }
